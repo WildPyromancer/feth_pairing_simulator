4f0b9200569780240f42ddfc9acf13b3eea8293b
WildPyromancer <lapisandsuger@gmail.com>
2024-10-27 09:22:05 +0900

    buildスクリプトの追加。

---
+++ cli/build_web.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+set -o errexit
+set -o noclobber
+set -o nounset
+set -o xtrace
+
+flet build web --module-name ./pairing_simulator.py \
+	--exclude '
+	.git,
+	.gitignore,
+	.vscode,
+	auto_py_to_exe_config.json,
+	cli,
+	CONTRIBUTING.adoc,
+	docs,
+	logs,
+	modules/_pychache_,
+	modules/.ruff_cache,
+	README.md,
+	resource,
+	tests,
+	.venv'
