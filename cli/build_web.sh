1fe2f363247a796fe90ffd00679a7039adb50788
WildPyromancer <lapisandsuger@gmail.com>
2024-10-28 15:49:43 +0900

    ビルドスクリプトの修正。

--- cli/build_web.sh
+++ cli/build_web.sh
@@ -3,14 +3,17 @@
 set -o errexit
 set -o noclobber
 set -o nounset
-set -o xtrace
+# set -o xtrace
 
+script_dir="$(dirname "${0}")"
+
+cd "$(readlink -f "$script_dir/../")"
 flet build web --module-name ./pairing_simulator.py \
 	--exclude '
 	.git,
 	.gitignore,
 	.ruff_cache,
-	.venv
+	.venv,
 	.vscode,
 	auto_py_to_exe_config.json,
 	cli,
@@ -23,3 +26,22 @@ flet build web --module-name ./pairing_simulator.py \
 	resource,
 	tests,
 '
+
+path_of_web_dir=./build/web
+path_of_app_zip="$path_of_web_dir/assets/app/app.zip"
+# CloudFlare Pages の1ファイル辺りのサイズ上限が25mb。
+echo "===== 24MB over files list ====="
+files_that_25mb_over="$(find "$path_of_web_dir" -size +24M)"
+echo "$files_that_25mb_over" | while read -r LINE; do
+	echo "$LINE"
+	if [ "$LINE" = $path_of_app_zip ]; then
+		zipinfo -l "$path_of_app_zip" |
+			sed '$d' |
+			awk 'NR>1{print $6,$10}' |
+			sort --numeric-sort --reverse |
+			head --lines=20 |
+			awk '{printf "%10s | %s\n", $1, $2}'
+	fi
+done
+
+test "$files_that_25mb_over" = ''
